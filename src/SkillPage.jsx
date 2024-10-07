6acadb5411d325a803b1db9bcd497c2aa4443f86
BryanSurjadi <bryanarmando999@gmail.com>
2024-10-07 13:55:28 +0700

    update png

--- src/SkillPage.jsx
+++ src/SkillPage.jsx
@@ -6,16 +6,16 @@ import resume from "./assets/resume.pdf";
 // import react from "./src/assets/react.png";
 
 const skills = [
-  "./src/assets/html.png",
-  "./src/assets/css.png",
-  "./src/assets/js.png",
-  "./src/assets/react.png",
-  "./src/assets/express.png",
-  "./src/assets/figma.png",
-  "./src/assets/mongodb.png",
-  "./src/assets/firebase.png",
-  "./src/assets/java.png",
-  "./src/assets/git.png",
+  "/src/assets/html.png",
+  "/src/assets/css.png",
+  "/src/assets/js.png",
+  "/src/assets/react.png",
+  "/src/assets/express.png",
+  "/src/assets/figma.png",
+  "/src/assets/mongodb.png",
+  "/src/assets/firebase.png",
+  "/src/assets/java.png",
+  "/src/assets/git.png",
 ];
 export default function SkillPage() {
   return (
